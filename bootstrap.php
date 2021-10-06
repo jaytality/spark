855762625ae20f2fd4d1373848d686c95e48e9ef
jaytality <johnathan.tiong@gmail.com>
2021-10-06 11:38:02 +1100

    added illuminate db to bootstrap

--- bootstrap.php
+++ bootstrap.php
@@ -1,3 +1,30 @@
 <?php
 
-// bootstrap for any front-end strapping
+/**
+ * bootstrap for database setup and interaction
+ */
+
+use Illuminate\Database\Capsule\Manager as Capsule;
+
+/**
+ * Database Connection
+ */
+$capsule = new Capsule;
+
+$capsule->addConnection([
+    "driver"   => getConfig("database.type"),
+    "host"     => getConfig("database.host"),
+    "database" => getConfig("database.name"),
+    "username" => getConfig("database.user"),
+    "password" => getConfig("database.pass")
+]);
+
+$capsule->setAsGlobal();
+$capsule->bootEloquent();
+
+/**
+ * Database Schema
+ */
+foreach (glob(ROOT."/schema/*.php") as $filename) {
+    include $filename;
+}
