bbc6fc70e53289eecf83c6a78f17336a96806911
jaytality <johnathan.tiong@gmail.com>
2021-10-07 13:30:25 +1100

    crons processing stat

---
+++ crons.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * crons lets you create PHP scripts within the site that can run based off the cron setup on the host
+ *
+ * the expectation is this cron file is called _every minute_ and this file will check the crons/ folder for
+ * any scripts that need to run
+ *
+ * created: 2021-10-07
+ * updated:
+ *
+ * @author Johnathan Tiong <johnathan.tiong@gmail.com>
+ * @copyright 2021 Johnathan Tiong
+ */
