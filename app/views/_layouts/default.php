d415eca231d301780ecfe7ebc9b2b1c89a2662f3
jaytality <johnathan.tiong@gmail.com>
2025-06-21 07:41:38 +1000

    added default layout to be included with repo

---
+++ app/views/_layouts/default.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title><?=$viewOpts['page']['title']?></title>
+    
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
+    </head>
+    <body>
+        <h1>Hello, world!</h1>
+        <p>
+            Your content should go here!
+        </p>
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
+        <script src="https://kit.fontawesome.com/4ab642e904.js" crossorigin="anonymous"></script>
+    </body>
+</html>
