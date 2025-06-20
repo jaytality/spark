af24f0bb34cb21248866446586eedfca935e1c33
jaytality <johnathan.tiong@gmail.com>
2025-06-21 07:42:44 +1000

    updated documentation, changing menu section to be an array

--- library/core/Controller.php
+++ library/core/Controller.php
@@ -1,5 +1,13 @@
 <?php
-
+/**
+ * class: Controller
+ *
+ * Controller handling class
+ *
+ * @author Johnathan Tiong <johnathan.tiong@gmail.com>
+ * @since v1.0.2014.09.01
+ * @version v1.1.2025.06.21
+ */
 namespace spark\Core;
 
 class Controller
@@ -11,13 +19,13 @@ class Controller
     function __construct()
     {
         // default View Options
-        $this->viewOpts['page']['layout']  = 'default';
-        $this->viewOpts['page']['content'] = 'home/index';
-        $this->viewOpts['page']['title']   = 'Welcome';
+        $this->viewOpts['page']['layout']  = 'default';     // page layout template to use (empty to make a landing page)
+        $this->viewOpts['page']['content'] = 'home/index';  // page's content php file location
+        $this->viewOpts['page']['title']   = 'Default';     // browser title for the page
 
         $this->viewOpts['menu']['enabled'] = true;
         $this->viewOpts['menu']['content'] = 'main';
-        $this->viewOpts['menu']['section'] = 'home';
+        $this->viewOpts['menu']['section'] = [];
         $this->viewOpts['menu']['style']   = 'solid';
 
         $this->viewOpts['sidebar']['enabled'] = false;
